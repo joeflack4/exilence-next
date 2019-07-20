c9e3de2a9bd9770ed694bd6916851499c177a16f
Viktor Gullmark <viktor.gullmark@gmail.com>
2019-07-20 11:27:19 +0200

    Started working on auto updater service

--- ExilenceNextClient/main.ts
+++ ExilenceNextClient/main.ts
@@ -10,17 +10,17 @@ autoUpdater.logger = log;
 autoUpdater.logger.transports.file.level = 'info';
 log.info('App starting...');
 
-function sendStatusToWindow(text) {
-  log.info(text);
-  win.webContents.send('message', text);
-}
-
 Sentry.init({ dsn: 'https://81deb5bd2814402f9efd7db4cfd84fd2@sentry.io/1507995' });
 
 let win, serve;
 const args = process.argv.slice(1);
 const ipcMain = require('electron').ipcMain;
 serve = args.some(val => val === '--serve');
+
+ipcMain.on('statusToWindow', (text) => {
+  log.info(text);
+  win.webContents.send('message', text);
+});
 
 ipcMain.on('relaunch', () => {
   app.relaunch();
@@ -75,47 +75,6 @@ function createWindow() {
 }
 
 try {
-
-  autoUpdater.on('checking-for-update', () => {
-    sendStatusToWindow('Checking for update...');
-  });
-
-  autoUpdater.on('update-available', (info) => {
-    sendStatusToWindow('Update available.');
-  });
-
-  autoUpdater.on('update-not-available', (info) => {
-    sendStatusToWindow('Update not available.');
-  });
-
-  autoUpdater.on('error', (err) => {
-    sendStatusToWindow('Error in auto-updater. ' + err);
-  });
-
-  autoUpdater.on('download-progress', (progressObj) => {
-    let log_message = 'Download speed: ' + progressObj.bytesPerSecond;
-    log_message = log_message + ' - Downloaded ' + progressObj.percent + '%';
-    log_message = log_message + ' (' + progressObj.transferred + '/' + progressObj.total + ')';
-    sendStatusToWindow(log_message);
-  });
-
-  autoUpdater.on('update-downloaded', (event, releaseNotes, releaseName) => {
-    sendStatusToWindow('Update downloaded');
-
-    const dialogOpts = {
-      type: 'info',
-      buttons: ['Restart', 'Later'],
-      title: 'Application Update',
-      message: process.platform === 'win32' ? releaseNotes : releaseName,
-      detail: 'A new version has been downloaded. Restart the application to apply the updates.'
-    };
-
-    dialog.showMessageBox(dialogOpts, (response) => {
-      if (response === 0) { autoUpdater.quitAndInstall(); }
-    });
-
-  });
-
   // This method will be called when Electron has finished
   // initialization and is ready to create browser windows.
   // Some APIs can only be used after this event occurs.
